ab04d3f330c9655094ad0991e9476ca4a353c175
w4daka <ignorenegativeideology999@gmail.com>
2026-03-21 17:41:16 +0900

    feat(flake.nix) add

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "wc dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+
+            # Rust
+            rustc
+            cargo
+
+            # Diagram
+            mermaid-cli
+
+            # Slides
+            marp-cli
+          ];
+        };
+      }
+    );
+}
